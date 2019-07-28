2cbd974da20f0e731d9e9fa1af0a2a55f161459c
phsnandasena <phsnandasena060@gmail.com>
2019-07-28 12:32:47 +0530

    add reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer  } from "redux-form";
 import invoicReducer from './invoiceReducer'
+import invoiceItems from './invoiceItemReducer'
 export default combineReducers({
     invioces: invoicReducer,
-    form: formReducer
+    form: formReducer,
+    invoiceItems: invoiceItems
 
 });
